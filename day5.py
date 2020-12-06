54e878d72fb509d9a925b69534b50e21a33f37e2
seblars <47225156+seblars@users.noreply.github.com>
2020-12-06 21:20:03 +0000

    Update day5.py

--- day5.py
+++ day5.py
@@ -3,21 +3,21 @@ import numpy as np
 data = ''.join(fileinput.input()).split('\n')
 
 # part 1
-outputs = []
 row_quadrants = np.power(2, np.flip(np.arange(0,7)))
 col_quadrants = np.power(2, np.flip(np.arange(0,3)))
-    
+
+outputs = []
 for i in range(len(data)):
     row_chars = np.array([i for i in data[i][:7]])
     col_chars = np.array([i for i in data[i][7:]])
-    
+
     r, c = row_quadrants[row_chars == 'B'].sum(), col_quadrants[col_chars == 'R'].sum()
     sID = r*8 + c
- 
-    outputs.append([sID, r, c])
-
-print(np.max(np.array(outputs)[:,0]))
+    
+    outputs.append(sID)
+    
+print(max(outputs))
 
 # part 2
-idx = np.argmax(np.sort(outputs[:,0])[1:] - np.sort(outputs[:,0])[:-1])
-print(np.sort(outputs[:,0])[idx] + 1)
+idx = np.argmax(np.sort(outputs)[1:] - np.sort(outputs)[:-1])
+print(np.sort(outputs)[idx] + 1)

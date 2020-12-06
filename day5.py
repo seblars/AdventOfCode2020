0a679000c54cc1f29f3f43f8ebff89d83fa8a945
seblars <47225156+seblars@users.noreply.github.com>
2020-12-06 21:16:41 +0000

    Update day5.py

--- day5.py
+++ day5.py
@@ -10,15 +10,13 @@ col_quadrants = np.power(2, np.flip(np.arange(0,3)))
 for i in range(len(data)):
     row_chars = np.array([i for i in data[i][:7]])
     col_chars = np.array([i for i in data[i][7:]])
-
+    
     r, c = row_quadrants[row_chars == 'B'].sum(), col_quadrants[col_chars == 'R'].sum()
     sID = r*8 + c
-    
+ 
     outputs.append([sID, r, c])
-    
-outputs = np.array(outputs)
 
-print(np.max(outputs[:,0]))
+print(np.max(np.array(outputs)[:,0]))
 
 # part 2
 idx = np.argmax(np.sort(outputs[:,0])[1:] - np.sort(outputs[:,0])[:-1])
